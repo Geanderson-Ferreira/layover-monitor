67076303b30f69a79574d796349f44cad246c273
Geanderson Ferreira Alves <geandersonferreiralves@gmail.com>
2024-12-13 14:26:09 -0300

    Consertei a versao do psycopg

--- src/SQL_Functions.py
+++ src/SQL_Functions.py
@@ -1,16 +1,28 @@
-import psycopg2
+import psycopg
 from os import environ
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def execute_select_query(query):
-    connection = psycopg2.connect(database=environ['DATA_BASE_NAME'], 
-                                user=environ['DATA_BASE_USER'],
-                                host=environ['DATA_BASE_URL'],
-                                password=environ['DATA_BASE_PASSWORD'],
-                                port=environ['DATA_BASE_PORT'])
+    # connection = psycopg.connect(database=environ['DATA_BASE_NAME'], 
+    #                             user=environ['DATA_BASE_USER'],
+    #                             host=environ['DATA_BASE_URL'],
+    #                             password=environ['DATA_BASE_PASSWORD'],
+    #                             port=environ['DATA_BASE_PORT'])
 
+    # cursor = connection.cursor()
+
+    DATA_BASE_NAME = environ.get('DATA_BASE_NAME')
+    DATA_BASE_USER = environ.get('DATA_BASE_USER')
+    DATA_BASE_URL = environ.get('DATA_BASE_URL')
+    DATA_BASE_PASSWORD = environ.get('DATA_BASE_PASSWORD')
+    DATA_BASE_PORT = environ.get('DATA_BASE_PORT')
+
+    # Estabelecendo a conexão com o banco de dados
+    connection = psycopg.connect(
+        f"dbname={DATA_BASE_NAME} user={DATA_BASE_USER} password={DATA_BASE_PASSWORD} host={DATA_BASE_URL} port={DATA_BASE_PORT}"
+    )
     cursor = connection.cursor()
 
     # print("Banco de dados conectado.")
